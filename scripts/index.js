f0eadef36e15a845e1f4d9928dbc565ab7a218b1
stanken_a <artyemst3@mail.ru>
2022-11-30 20:10:46 +0300

    feat: add html markup, swipers logic

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".teachers__swiper", {
+  slidesPerView: 3,
+  initialSlide: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".teachers__slide-button-next",
+    prevEl: ".teachers__slide-button-prev",
+  },
+});
+
+var swiper = new Swiper(".feedback__swiper", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".feedback__pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + "</span>";
+    },
+  },
+  navigation: {
+    nextEl: ".feedback__slide-button-next",
+    prevEl: ".feedback__slide-button-prev",
+  },
+});
